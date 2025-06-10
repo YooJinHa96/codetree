5ab9e9a2b0b06540202df8ad1f3112f1086dae84
YooJinHa96 <55770865+YooJinHa96@users.noreply.github.com>
2025-06-10 18:49:57 +0900

    Title: 최고의 13위치 2, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250610/최고의 13위치 2/best-place-of-13-2.js
+++ 250610/최고의 13위치 2/best-place-of-13-2.js
@@ -8,14 +8,22 @@ const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 // Please write your code here.
 let result = -1;
 for(let i =0 ; i < grid.length ; i++){
-    let firstMax=-1;
-    for(let j=0 ; j < grid.length-2 ; j++){
-        firstMax=Math.max(grid[i][j]+ grid[i][j+1] + grid[i][j+2])
-    }
-    for(let j=i+1; j<grid.length ; j++){
-        for(let k=0; k < grid.length-2; k++){
-            result = Math.max(result, firstMax+ grid[j][k]+grid[j][k+1]+grid[j][k+2])
+    let first=-1;
+    for(let j =0  ;j<grid.length- 2 ; j++){
+        first = grid[i][j]+grid[i][j+1]+grid[i][j+2]
+        for(let k=i ; k < grid.length;k++){
+            if(i===k){
+                for(let l = j +3 ;l<grid.length-2;l++){
+                    result = Math.max(result, first+ grid[k][l]+grid[k][l+1]+grid[k][l+2])
+                }
+            }else{
+                 for(let l =0 ;l<grid.length-2;l++){
+                    result = Math.max(result, first+ grid[k][l]+grid[k][l+1]+grid[k][l+2])
+                }
+            }
+
         }
     }
+    
 }
 console.log(result)
