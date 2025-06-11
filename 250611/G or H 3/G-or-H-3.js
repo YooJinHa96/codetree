d290b7af954f5bad542e3a56872ac18a6411195d
YooJinHa96 <55770865+YooJinHa96@users.noreply.github.com>
2025-06-11 10:21:24 +0900

    Title: G or H 3, Time: 194ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250611/G or H 3/G-or-H-3.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const people = [];
+for (let i = 1; i <= n; i++) {
+  const [x, c] = input[i].split(' ');
+  people.push([Number(x), c]);
+}
+let maxLen = 0 ;
+for(let i =0 ; i < n;i++){
+    const [x, c] = people[i];
+    maxLen=Math.max(maxLen, x)
+}
+const arr = new Array(maxLen).fill(0);
+for(let i= 0 ; i < n;i++){
+    const [x, c] =people[i];
+    if(c==='G'){
+        arr[x-1]=1
+    }
+    else if(c==='H'){
+        arr[x-1]=2
+    }
+}
+let result = 0;
+
+// Please Write your code here.
+for(let i =0 ; i < n -k +1 ;i++){
+    let count =0 ;
+    for(let j=i; j<i+k;j++){
+        count+=arr[j];
+    }
+    result =Math.max(result, count)
+}
+
+console.log(result)
