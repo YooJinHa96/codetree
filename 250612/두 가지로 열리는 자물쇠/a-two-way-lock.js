b7defb367a79a77f7427cb5116af0ee9e800bd7c
YooJinHa96 <55770865+YooJinHa96@users.noreply.github.com>
2025-06-12 10:56:13 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 225ms, Memory: 19MB, Status: Passed - Codetree

--- 250612/두 가지로 열리는 자물쇠/a-two-way-lock.js
+++ 250612/두 가지로 열리는 자물쇠/a-two-way-lock.js
@@ -1,59 +1,32 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 입력 처리
 const n = Number(input[0]);
-const password = input[1].split(' ').map(Number);
-const password2 = input[2].split(' ').map(Number);
+const [a1, b1, c1] = input[1].split(' ').map(Number);
+const [a2, b2, c2] = input[2].split(' ').map(Number);
 
-
-// Please Write your code here.
-const wrap = (x, n) => ((x - 1 + n) % n) + 1;
-let triplets =[];
-let triplets2=[]
-
-for(let q =0 ;q < 3 ; q++){
-    let t=new Set()
-    let t2=new Set();
-    for(let i=password[q]-2 ; i<=password[q]+2; i++){
-        t.add(wrap(i,n))
-    }   
-     for(let i=password2[q]-2 ; i<=password2[q]+2; i++){
-        t2.add(wrap(i,n))   
-    }     
-    triplets.push([...t])
-    triplets2.push([...t2])  
+// 거리 함수 (원형 거리)
+function isClose(x, y) {
+  const diff = Math.abs(x - y);
+  return Math.min(diff, n - diff) <= 2;
 }
-function generateCombinations(arr) {
-  const set = new Set();
-  for (let a of arr[0]) {
-    for (let b of arr[1]) {
-      for (let c of arr[2]) {
-        set.add(`${a} ${b} ${c}`);
+
+// 조건을 만족하는지 확인하는 함수
+function isValid(x, y, z, ref) {
+  return isClose(x, ref[0]) && isClose(y, ref[1]) && isClose(z, ref[2]);
+}
+
+const seen = new Set();
+
+for (let x = 1; x <= n; x++) {
+  for (let y = 1; y <= n; y++) {
+    for (let z = 1; z <= n; z++) {
+      if (isValid(x, y, z, [a1, b1, c1]) || isValid(x, y, z, [a2, b2, c2])) {
+        seen.add(`${x},${y},${z}`);
       }
     }
   }
-  return set;
 }
 
-const set1 = generateCombinations(triplets);
-const set2 = generateCombinations(triplets2);
-
-// 교집합 개수 구하기
-let count = 0;
-for (let combo of set1) {
-  if (set2.has(combo)) count++;
-}
-
-
-
-let total  = 0;
-let product=1;
-triplets.map((arr,index)=>{
-    product*=arr.length;
-})
-let product2=1;
-triplets2.map((arr,index)=>{
-    product2*=arr.length
-})
-total=product+product2;
-console.log(total - count)
+console.log(seen.size);
