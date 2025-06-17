c11221790dbacfd3d6714a1daeae4a8d8289cc71
YooJinHa96 <55770865+YooJinHa96@users.noreply.github.com>
2025-06-17 13:52:47 +0900

    Title: 겹치지 않는 선분 2, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250617/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
+++ 250617/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
@@ -1,37 +1,24 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-
 const n = Number(input[0]);
-const segments = input.slice(1, n + 1).map(line => {
-  const [x1, x2] = line.split(' ').map(Number);
-  return [{ x: x1, y: 0 }, { x: x2, y: 1 }];
-});
+const segments = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+// Please Write your code here.
+let result =0;
 
-// ccw 함수 (Counter Clockwise)
-function ccw(a, b, c) {
-  const cross = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-  return cross > 0 ? 1 : cross < 0 ? -1 : 0;
-}
+for(let i=0 ; i <n;i++){
+    let flag=false
+    for(let j =0 ;j<n; j++){
+        if(i!==j){
+            if((segments[i][0]<=segments[j][0] && segments[i][1]>=segments[j][1])||
+            (segments[i][0]>=segments[j][0]&&segments[i][1]<=segments[j][1])
+            ){
+                flag=true;
+                break;
+            }
 
-// 두 선분이 교차하는지 판별
-function isIntersect(a1, a2, b1, b2) {
-  const ab = ccw(a1, a2, b1) * ccw(a1, a2, b2);
-  const ba = ccw(b1, b2, a1) * ccw(b1, b2, a2);
-  return ab < 0 && ba < 0;
-}
-
-const intersected = new Array(n).fill(false);
-
-for (let i = 0; i < n; i++) {
-  for (let j = i + 1; j < n; j++) {
-    const [a1, a2] = segments[i];
-    const [b1, b2] = segments[j];
-    if (isIntersect(a1, a2, b1, b2)) {
-      intersected[i] = true;
-      intersected[j] = true;
+        }
+    
     }
-  }
+    if(!flag)result++;
 }
-
-const result = intersected.filter(x => !x).length;
-console.log(result);
+console.log(result)
